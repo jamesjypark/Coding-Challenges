4ccbfeecba6a7fb9a834df2d6e127b6e3b49f6f0
Juyoung Park <james.park.victoria@gmail.com>
2019-07-24 20:17:19 -0700

    Longest palindrome in an array with O(n) time complexity

--- LongestPalindromicSubset.js
+++ LongestPalindromicSubset.js
@@ -6,34 +6,35 @@
  * @param {string} s
  * @return {string}
  * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ * Implementation using helper function to compute longest palindrome at each index
  *
- * Solution algorithm adapted from jinwu's Java Solution for better performance
  */
 
-let start = 0,
-  maxLength = 0;
-
-longestPalindrome = s => {
-  if (s.length < 2) {
-    return s;
+const computePalindrome = (i, str) => {
+  let ptr1 = i;
+  let ptr2 = i;
+  while (str[ptr2 + 1] === str[i]) {
+    ptr2++;
   }
-
-  for (let i = 0; i < s.length - 1; i++) {
-    expandPalindrome(s, i, i);
-    expandPalindrome(s, i, i + 1);
+  while (str[ptr1 - 1] && str[ptr2 + 1] && str[ptr1 - 1] === str[ptr2 + 1]) {
+    ptr1--;
+    ptr2++;
   }
-  console.log(`start: ${start}`);
-  console.log(`maxLength: ${maxLength}`);
-  return s.slice(start, start + maxLength);
+  return str.slice(ptr1, ptr2 + 1);
 };
 
-expandPalindrome = (str, i, j) => {
-  while (i >= 0 && j < str.length && str[i] === str[j]) {
-    i--;
-    j++;
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const longestPalindrome = s => {
+  let maxPalindrome = "";
+  for (let i = 0; i < s.length; i++) {
+    let currPalindrome = computePalindrome(i, s);
+    if (currPalindrome.length > maxPalindrome.length) {
+      maxPalindrome = currPalindrome;
+    }
   }
-  if (j - i > maxLength + 1) {
-    start = i + 1;
-    maxLength = j - i - 1;
-  }
+
+  return maxPalindrome;
 };
